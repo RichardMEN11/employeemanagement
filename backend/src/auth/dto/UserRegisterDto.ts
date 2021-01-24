507ec50d55f38d85dca5808a2ee5bee52fb513b4
Richard <richardmenning@googlemail.com>
2021-01-24 19:59:18 +0100

    add user creation methods

---
+++ backend/src/auth/dto/UserRegisterDto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class UserRegisterDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly lastName: string;
+
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsString()
+  @MinLength(6)
+  readonly password: string;
+}
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { UserRegisterDto } from '../auth/dto/UserRegisterDto';
+import { UserRepository } from './user.repository';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(public readonly userRepository: UserRepository) {}
+
+  async createUser(userRegisterDto: UserRegisterDto) {
+    const user = this.userRepository.create(userRegisterDto);
+    return user;
+  }
+}
