5c779675df838dfa6356c4ea776b9d3a3d7f1067
Richard <richardmenning@googlemail.com>
2021-01-25 08:24:47 +0100

    fix wrong imports

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
+import { UserService } from 'src/user/user.service';
+import { UserRepository } from 'src/user/user.repository';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [TypeOrmModule.forFeature([UserRepository])],
+  providers: [AuthService, UserService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { UserRepository } from './user.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserRepository])],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  exports: [UserService, UserModule],
 })
 export class UserModule {}
