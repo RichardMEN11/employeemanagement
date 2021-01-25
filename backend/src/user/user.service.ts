11c3ddf16d2c3afbdd0efc46bb4b340d303ddf94
Richard <richardmenning@googlemail.com>
2021-01-25 22:17:45 +0100

    use the save method to save data

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { UserRegisterDto } from '../auth/dto/UserRegisterDto';
 import { UserRepository } from './user.repository';
+import { FindConditions } from 'typeorm';
+import { UserEntity } from './user.entity';
 
 @Injectable()
 export class UserService {
@@ -8,6 +10,12 @@ export class UserService {
 
   async createUser(userRegisterDto: UserRegisterDto) {
     const user = this.userRepository.create(userRegisterDto);
+    await this.userRepository.save(user);
+    return user;
+  }
+
+  async findOneByMail(email: string): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({ where: { email } });
     return user;
   }
 }
